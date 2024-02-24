8c5c5dd74c90990b5f56897d151ae33e2ff49e06
Irene VS <147383929+iireneVS@users.noreply.github.com>
2024-02-24 11:49:41 +0000

    wip: clase

--- src/app.py
+++ src/app.py
@@ -8,42 +8,78 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Users
 
+
+# Instancias Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+# Configuracion de DB
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+
+@app.route('/hello', methods=['GET'])
 def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
+    response_body = {"msg": "Hello, this is your GET /user response "}
     return jsonify(response_body), 200
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def handle_users():
+    response_body = results = {}
+    # Metodo GET
+    if request.method == 'GET':
+        users = db.session.execute(db.select(Users)).scalars()
+        response_body['results'] = [row.serialize() for row in users]
+        response_body['message'] = 'Metodo de GET de users'
+        return response_body, 200
+    # Metodo POST
+    if request.method == 'POST':
+        user = Users(email = data['email'],
+                     password = data['password'],
+                     is_active = True)           
+        db.session.add(user)
+        db.session.commit()
+        response_body['results'] = user.serialize()
+        response_body['message'] = 'Metodo de POST de users'
+        return response_body, 200
+
+
+@app.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])       
+def handle_user(id):
+    response_body = {}
+    print(id)
+    if request.method == 'GET':
+        response_body['message'] = 'metodo GET del users/<id>'
+        return response_body, 200
+    if request.method == 'PUT':
+        response_body['message'] = 'metodo PUT del users/<id>'
+        return response_body, 200
+    if request.method == 'DELETE':
+        response_body['message'] = 'metodo DELETE del users/<id>'
+        return response_body, 200
+     
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
